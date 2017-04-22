d7a25d0ede1a1dacd4e886ecdba30dbfd39d9533
Przemyslawmd <przemyslawmd@gmail.com>
2017-04-22 19:47:01 -0400

    Test for generating board added

--- Test/testfifteen.cpp
+++ Test/testfifteen.cpp
@@ -8,38 +8,83 @@ class TestFifteen : public QObject
 {
     Q_OBJECT
 
-public:
-    TestFifteen();
-
 private Q_SLOTS:
-    void testCase();
+
+    void testCaseCreatingBoard();
+    void testCreatingBoard( int size );
+
+    void testCaseGeneratingBoard();
+    void testGeneratingBoard( int size );
 };
 
-TestFifteen::TestFifteen()
+void TestFifteen::testCaseCreatingBoard()
 {
+    testCreatingBoard( 4 );
+    testCreatingBoard( 6 );
 }
 
-void TestFifteen::testCase()
-{
-    Board board(4);
 
+void TestFifteen::testCaseGeneratingBoard()
+{
+    testGeneratingBoard( 5 );
+    testGeneratingBoard( 7 );
+}
+
+/**********************************************/
+/* TEST CREATING BOARD ************************/
+// Test creating board at the beginning
+// Board should be created and solved
+
+void TestFifteen::testCreatingBoard( int size )
+{
+    Board board( size );
     int** squares = board.sendBoard();
 
     int k = 1;
 
-    for ( int i = 0; i < 4; i++)
+    for ( int i = 0; i < size; i++)
     {
-        for ( int j = 0; j < 4; j++)
+        for ( int j = 0; j < size; j++)
         {
-            if ( i != 3 || j != 3 )
-                QCOMPARE( squares[i][j], k++);
+            if ( i != ( size -1 ) || j != ( size - 1 ) )
+                QCOMPARE( squares[i][j], k++ );
             else
-                QCOMPARE( squares[i][j], 0);
+                QCOMPARE( squares[i][j], 0 );
+        }
+    }
+}
+
+
+/**********************************************/
+/* TEST GENERATING BOARD **********************/
+// Test generating board
+// Values are not sorted
+
+void TestFifteen::testGeneratingBoard( int size )
+{
+    Board board( size );
+    int** squares = board.randomBoard();
+
+    QList<int> values;
+
+    for ( int i = 0; i < ( size * size ); i++ )
+        values.append( i );
+
+
+    for ( int i = 0; i < size; i++)
+    {
+        for ( int j = 0; j < size; j++)
+        {
+                // Check whether value exists in a list
+                QCOMPARE( values.contains( squares[i][j]), true );
+                values.removeOne( squares[i][j] );
         }
     }
 
-    QVERIFY2(true, "Failure");
+    // List should be empty at the end
+    QCOMPARE( values.size(), 0 );
 }
+
 
 QTEST_APPLESS_MAIN(TestFifteen)
 
