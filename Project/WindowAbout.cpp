4d6e40ec724d3c8d9e98776d9b58aa41a90a6054
Przemyslawmd <przemyslawmd@gmail.com>
2017-04-29 19:09:25 -0400

    Unnecessary lines removed

--- Project/WindowAbout.cpp
+++ Project/WindowAbout.cpp
@@ -1,24 +1,22 @@
+
 #include "WindowAbout.h"
 
 WindowAbout::WindowAbout()
 {
     setModal( true );
-    setGeometry( 100, 100, 490, 180 );
-    setMaximumSize( 490, 180 );
-    setWindowTitle( " ");
+    setGeometry( 100, 100, 520, 200 );
+    setMaximumSize( 520, 200 );
+    setWindowTitle( " " );
 
     QVBoxLayout layout( this );
     QString html;
 
-    HTML( "<html><head><style> p {margin-left:15px; line-height:20px;} </style></head><body><br/>" );
-    HTML( "<pre>  Version:      1.2.0" );
-    HTML( "<pre>  Author:       Przemyslaw Madej, Cracow 2016</pre>" );
+    HTML( "<br/><pre>  Version:      1.2.1" );
+    HTML( "<pre>  Author:       Przemyslaw Madej, Warsaw 2017</pre>" );
     HTML( "<pre>  Page:         http://przemeknet.pl/fifteenEn.aspx</pre>" );
-    HTML( "</body></html>" );
 
     doc.setHtml( html );
     browser.setDocument( &doc );
     layout.addWidget( &browser );
     show();
 }
-
