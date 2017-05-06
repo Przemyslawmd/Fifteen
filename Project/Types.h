6ff26caf957f78c22765bd870edf33dfa311a1ae
Przemyslawmd <przemyslawmd@gmail.com>
2017-05-06 18:19:30 -0400

    Changes in Types.h according to changes in two commits before

--- Project/Types.h
+++ Project/Types.h
@@ -1,15 +1,14 @@
+
 #ifndef TYPES_H
 #define TYPES_H
 
 // Not strongly typed enums because these enums are used for indexing an array
 
-// BLUE has value 2 because of an array of radio controls in WindowSetting class
-enum Color { BLUE = 2, GREEN, RED };
+enum Color { BLUE, GREEN, RED };
 
-enum Level { FOUR = 4, FIVE, SIX, SEVEN };
+enum BoardSize { FOUR = 4, FIVE, SIX, SEVEN };
 
 enum SquareSize { _50 = 50, _75 = 75, _100 = 100, _125 = 125, _150 = 150 };
 
 #endif // TYPES_H
-
 
