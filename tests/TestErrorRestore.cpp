0077b6c3d3d5152788cbb92440c877b259855b58
przemyslawmd <przemyslawmd@gmail.com>
2022-09-15 15:18:13 +0200

    Tests for errors when board is loaded from a file

---
+++ tests/TestErrorRestore.cpp
@@ -0,0 +1,57 @@
+
+#include "config.h"
+
+#include "../src/Board.h"
+#include "../src/Types.h"
+#include "../src/FileBoard/IOBoard.h"
+#include "../src/GraphicBoard/ImageProvider.h"
+
+#include <gtest/gtest.h>
+
+#include <QString>
+
+
+void runTestErrorRestore(std::string fileName, QString errorLog)
+{
+    std::string filePath = IMPROPER_BOARD_DIR + std::string(fileName);
+    
+    IOBoard io;
+    auto readValues = io.readBoardFromFile(QString::fromStdString(filePath));
+    
+    EXPECT_EQ(readValues, nullptr);
+
+    QString message = Message::getMessages();
+    EXPECT_EQ(message, errorLog);
+}
+
+
+TEST(testErrorRestore, BoardSizeError)
+{
+    runTestErrorRestore("improperBoardSize", "Error reading file : size of a board");
+}
+
+TEST(testErrorRestore, BoardModeError)
+{
+    runTestErrorRestore("improperBoardMode", "Error reading file : kind of a board");
+}
+
+TEST(testErrorRestore, BoardValuesError)
+{
+    runTestErrorRestore("improperBoardValues", "Error reading file : board values");
+}
+
+TEST(testErrorRestore, BoardNotAllValues)
+{
+    runTestErrorRestore("improperBoardNotAllValues", "Error reading file : board values");
+}
+
+TEST(testErrorRestore, BoardTileSizeError)
+{
+    runTestErrorRestore("improperBoardTileSize", "Error reading file : tile size");
+}
+
+TEST(testErrorRestore, BoardNotAllGraphicData)
+{
+    runTestErrorRestore("improperBoardNotAllGraphicData", "Error reading file : images");
+}
+
--- tests/config.h
+++ tests/config.h
@@ -2,4 +2,5 @@
 #pragma once
 
 #define ROOT_DIR "/home/przemek/Projects/Fifteen/tests/"
+#define IMPROPER_BOARD_DIR "/home/przemek/Projects/Fifteen/tests/improperBoards/"
 #define IMAGES_DIR "/home/przemek/Projects/Fifteen/tests/images/"
