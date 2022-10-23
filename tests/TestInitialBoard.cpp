8c87ca5023da4cc0d7654a25d399de83fed701bd
przemyslawmd <przemyslawmd@gmail.com>
2022-10-24 00:35:14 +0200

    Tests little refactoring

--- tests/TestInitialBoard.cpp
+++ tests/TestInitialBoard.cpp
@@ -9,11 +9,11 @@
 void runTestInitialBoard(BoardSize boardSize)
 {
     auto board = std::make_unique<Board>(boardSize );
-    auto values = board->sendBoard();
+    auto const& values = board->sendBoard();
     uint sizeInt = Maps::boardSizeInt.at(boardSize);
     uint tilesCount = sizeInt * sizeInt;
-
-    EXPECT_EQ(tilesCount, values.size());
+    
+    ASSERT_EQ(tilesCount, values.size());
     for (uint i = 0; i < tilesCount - 1; i++)
     {
         EXPECT_EQ(values[i], i);
@@ -21,22 +21,23 @@ void runTestInitialBoard(BoardSize boardSize)
     EXPECT_EQ(values[tilesCount - 1], board->getEmptyTile());   
 }
 
-TEST(testInitialBoard, Four)
+
+TEST(testInitialBoard, BoardSizeFour)
 {
     runTestInitialBoard(BoardSize::FOUR);
 }
 
-TEST(testInitialBoard, Five)
+TEST(testInitialBoard, BoardSizeFive)
 {
     runTestInitialBoard(BoardSize::FIVE);
 }
 
-TEST(testInitialBoard, Six)
+TEST(testInitialBoard, BoardSizeSix)
 {
     runTestInitialBoard(BoardSize::SIX);
 }
 
-TEST(testInitialBoard, Seven)
+TEST(testInitialBoard, BoardSizeSeven)
 {
     runTestInitialBoard(BoardSize::SEVEN);
 }
--- tests/TestMoves.cpp
+++ tests/TestMoves.cpp
@@ -11,15 +11,15 @@
 #include <vector>
 
     
-void runTestMoves(std::vector<int>& moves, std::vector<uint>& expectedValues, BoardSize boardSize)
+void runTestMoves(const std::vector<int>& moves, const std::vector<uint>& expectedValues, BoardSize boardSize)
 {
     auto board = std::make_unique<Board>(boardSize);
-    for(int move : moves )
+    for (int move : moves)
     {
-        board->checkMove(move/10, move%10);
+        board->checkMove(move / 10, move % 10);
     }
     
-    auto values = board->sendBoard();
+    auto const& values = board->sendBoard();
     for (int i = 0; i < values.size(); ++i)
     {
         EXPECT_EQ(values[i], expectedValues[i]);    
@@ -27,17 +27,17 @@ void runTestMoves(std::vector<int>& moves, std::vector<uint>& expectedValues, Bo
 }
 
 
-TEST(testMoves, Four)
+TEST(testMoves, BoardSizeFour)
 {
-    std::vector< int > moves { 23, 23, 22, 10, 11, 21, 22, 32, 33, 23, 22, 21, 20 };
-    std::vector< uint > expectedTiles { 0, 1, 2, 3, 4, 5, 6, 7, 15, 8, 9, 14, 12, 13, 11, 10 };
-    runTestMoves(moves, expectedTiles, BoardSize::FOUR);
+    runTestMoves({ 23, 23, 22, 10, 11, 21, 22, 32, 33, 23, 22, 21, 20 },
+                 { 0, 1, 2, 3, 4, 5, 6, 7, 15, 8, 9, 14, 12, 13, 11, 10 },
+                 BoardSize::FOUR);
 }
 
-TEST(testMoves, Five)
+TEST(testMoves, BoardSizeFive)
 {
-    std::vector< int > moves { 34, 24, 24, 11, 14, 13, 12, 02, 03, 02, 01, 00, 00, 10, 11, 21, 31, 30, 40, 41 };
-    std::vector< uint > expectedTiles { 5, 0, 1, 3, 4, 6, 11, 2, 7, 8, 10, 16, 12, 13, 9, 20, 15, 17, 18, 14, 21, 24, 22, 23, 19 };
-    runTestMoves(moves, expectedTiles, BoardSize::FIVE);
+    runTestMoves({ 34, 24, 24, 11, 14, 13, 12, 02, 03, 02, 01, 00, 00, 10, 11, 21, 31, 30, 40, 41 },
+                 { 5, 0, 1, 3, 4, 6, 11, 2, 7, 8, 10, 16, 12, 13, 9, 20, 15, 17, 18, 14, 21, 24, 22, 23, 19 }, 
+                 BoardSize::FIVE);
 }
 
