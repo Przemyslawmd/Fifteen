bfbf9b577580720382fbdf05139219339d8fd599
przemyslawmd <przemyslawmd@gmail.com>
2023-07-05 00:57:31 +0200

    File generated by CMake removed from repository

--- tests/config.h
+++
@@ -1,6 +0,0 @@
-
-#pragma once
-
-#define ROOT_DIR "/home/przemek/projects/Fifteen/tests/"
-#define IMPROPER_BOARD_DIR "/home/przemek/projects/Fifteen/tests/improperBoards/"
-#define IMAGES_DIR "/home/przemek/projects/Fifteen/tests/images/"
