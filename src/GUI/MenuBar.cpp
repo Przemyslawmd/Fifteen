949ae5f597a9c6451554b410bf219f6dabf8cd8c
przemyslawmd <przemyslawmd@gmail.com>
2025-01-23 18:01:00 +0100

    [MenuBar] It's unecessary to store QAction as a pointer

--- src/GUI/MenuBar.cpp
+++ src/GUI/MenuBar.cpp
@@ -4,9 +4,9 @@
 
 void MenuBar::bindSlot( ActionMenu actionMenu, std::function<void( void )> slot, QMainWindow* mainWindow, const QString& text )
 {
-    mapAction[ actionMenu ] = new QAction( mainWindow );
-    mapAction.at( actionMenu )->setText( text );
-    QObject::connect( mapAction.at( actionMenu ), &QAction::triggered, mainWindow, slot );
+    auto [it, _] = mapAction.emplace( actionMenu, mainWindow );
+    it->second.setText( text );
+    QObject::connect( &it->second, &QAction::triggered, mainWindow, slot );
 }
 
 /*********************************************************************************/
@@ -17,19 +17,19 @@ QMenuBar* MenuBar::createMenuBar( QMainWindow* mainWindow )
     QMenu* fileMenu = new QMenu( "File", mainWindow );
     fileMenu->setStyleSheet( "padding-left:10px;" );
 
-    fileMenu->addAction( mapAction.at( ActionMenu::OPEN_GRAPHIC ));
+    fileMenu->addAction( &mapAction.at( ActionMenu::OPEN_GRAPHIC ));
     fileMenu->addSeparator();
-    fileMenu->addAction( mapAction.at( ActionMenu::REM_GRAPHIC ));
-    mapAction.at( ActionMenu::REM_GRAPHIC )->setEnabled( false );
+    fileMenu->addAction( &mapAction.at( ActionMenu::REM_GRAPHIC ));
+    mapAction.at( ActionMenu::REM_GRAPHIC ).setEnabled( false );
     fileMenu->addSeparator();
-    fileMenu->addAction( mapAction.at( ActionMenu::SAVE_BOARD ));
+    fileMenu->addAction( &mapAction.at( ActionMenu::SAVE_BOARD ));
     fileMenu->addSeparator();
-    fileMenu->addAction( mapAction.at( ActionMenu::LOAD_BOARD ));
+    fileMenu->addAction( &mapAction.at( ActionMenu::LOAD_BOARD ));
 
     QMenuBar* menuBar = new QMenuBar( mainWindow );
     menuBar->addMenu( fileMenu );
-    menuBar->addAction( mapAction.at( ActionMenu::SETTINGS ));
-    menuBar->addAction( mapAction.at( ActionMenu::ABOUT ));
+    menuBar->addAction( &mapAction.at( ActionMenu::SETTINGS ));
+    menuBar->addAction( &mapAction.at( ActionMenu::ABOUT ));
     menuBar->setStyleSheet( "padding-left: 5px; margin: 3px;" );
     return menuBar;
 }
@@ -39,6 +39,6 @@ QMenuBar* MenuBar::createMenuBar( QMainWindow* mainWindow )
 
 void MenuBar::setActionMenuState( ActionMenu actionMenu, bool state )
 {
-    mapAction.at( actionMenu )->setEnabled( state );
+    mapAction.at( actionMenu ).setEnabled( state );
 }
 
